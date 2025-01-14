ad90c8e08c658fcb71c01a26f08abdfc3c268e99
Huy Tuấn <163986199+hahuytuan033@users.noreply.github.com>
2025-01-15 05:46:54 +0700

    add jumping with the K key and a button on the screen

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Rigidbody2D rb;
     public float movement;
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpHeight = 5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -17,10 +19,25 @@ public class Player : MonoBehaviour
     {
         movement = Input.GetAxis("Horizontal");
         movement = SimpleInput.GetAxis("Horizontal");
+
+        // Jump with K key
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            JumpButton();
+        }
     }
 
     private void FixedUpdate()
     {
         transform.position += new Vector3(movement, 0f, 0f) * Time.deltaTime * speed;
     }
+
+    //Jump with Jump Button
+    public void JumpButton()
+    {
+        Vector2 velocity = rb.linearVelocity;
+        velocity.y = jumpHeight;
+        rb.linearVelocity= velocity;
+    }
+    
 }
