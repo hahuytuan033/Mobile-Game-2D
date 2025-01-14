88daecb5ac32f77ddceff392842c8bc01363c830
Huy Tuấn <163986199+hahuytuan033@users.noreply.github.com>
2025-01-15 03:32:05 +0700

    create a script to allow the Player to move horizontally.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float movement;
+    [SerializeField] private float speed = 5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += new Vector3(movement, 0f, 0f) * Time.deltaTime *speed;
+    }
+}
