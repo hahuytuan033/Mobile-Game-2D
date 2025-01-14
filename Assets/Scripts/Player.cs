11db75f2ad6fe2c5a90597ec21d11a643d94fe69
Huy Tuấn <163986199+hahuytuan033@users.noreply.github.com>
2025-01-15 03:42:14 +0700

    add move the Player with SimpleInPut

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,17 +9,18 @@ public class Player : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         movement = Input.GetAxis("Horizontal");
+        movement = SimpleInput.GetAxis("Horizontal");
     }
 
     private void FixedUpdate()
     {
-        transform.position += new Vector3(movement, 0f, 0f) * Time.deltaTime *speed;
+        transform.position += new Vector3(movement, 0f, 0f) * Time.deltaTime * speed;
     }
 }
